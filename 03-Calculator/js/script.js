43300ad3b2ba63b2f2c67a25096a1bf8dee97346
Ethan Rouimi <ethanrv2003@gmail.com>
2023-01-27 20:29:55 +0100

    javascript implementation

---
+++ 03-Calculator/js/script.js
@@ -0,0 +1,84 @@
+num0 = document.getElementById("0");
+num1 = document.getElementById("1");
+num2 = document.getElementById("2");
+num3 = document.getElementById("3");
+num4 = document.getElementById("4");
+num5 = document.getElementById("5");
+num6 = document.getElementById("6");
+num7 = document.getElementById("7");
+num8 = document.getElementById("8");
+num9 = document.getElementById("9");
+numPlus = document.getElementById("plus");
+numMinus = document.getElementById("minus");
+numMultiply = document.getElementById("mult");
+numDivide = document.getElementById("divide");
+numEquals = document.getElementById("equal");
+clear = document.getElementById("clear");
+
+
+display = document.querySelector(".display__input");
+
+function for_display(num) {
+
+  if (num.innerText === "0") {
+    if (display.innerHTML === "0") {
+      display.innerHTML = "0";
+    } else {
+      display.innerHTML = display.innerHTML + num.innerText;
+    }
+
+  } else {
+    if (display.innerHTML === "0") {
+      display.innerHTML = num.innerText;
+    } else {
+      display.innerHTML = display.innerHTML + num.innerText;
+    }
+  }
+}
+
+
+
+clear.addEventListener("click", function() {
+  display.innerHTML = "0";
+});
+
+num0.addEventListener("click", function() {
+  for_display(num0);
+
+})
+
+num1.addEventListener("click", function() {
+  for_display(num1);
+});
+
+num2.addEventListener("click", function() {
+  for_display(num2);
+});
+
+num3.addEventListener("click", function() {
+  for_display(num3);
+});
+
+num4.addEventListener("click", function() {
+  for_display(num4);
+});
+
+num5.addEventListener("click", function() {
+  for_display(num5);
+});
+
+num6.addEventListener("click", function() {
+  for_display(num6);
+});
+
+num7.addEventListener("click", function() {
+  for_display(num7);
+});
+
+num8.addEventListener("click", function() {
+  for_display(num8);
+});
+
+num9.addEventListener("click", function() {
+  for_display(num9);
+});
